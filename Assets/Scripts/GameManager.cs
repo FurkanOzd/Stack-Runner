84eb7a822aec2ba6ccc05b69fb7e70b676ee5f9e
Ibrahim Furkan Ozdemir <58518824+furkanozd@users.noreply.github.com>
2023-10-29 04:25:27 +0300

    game mechanic wip

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,39 @@
+using PathBlocksModule;
+using UnityEngine;
+using Zenject;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _blockParent;
+
+    [SerializeField]
+    private PlayerController _playerController;
+
+    [SerializeField]
+    private Block _baseBlock;
+
+    [SerializeField]
+    private float _moveDuration;
+    [SerializeField]
+    private float _xSpawnOffset;
+
+    [SerializeField]
+    private Material[] _blockMaterials;
+
+    [Inject]
+    private BlockController _blockController;
+    
+    private void Start()
+    {
+        _blockController.Initialize(_blockParent, _xSpawnOffset, _moveDuration, _blockMaterials, _baseBlock);
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            _blockController.FitBlock();
+        }
+    }
+}
